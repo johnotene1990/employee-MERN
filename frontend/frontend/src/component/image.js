1b9e9b807a73c3bc949deb72895535a0359c956b
johnotene1990 <johnotene1990@gmail.com>
2024-08-26 12:31:22 -0700

    my first commit

---
+++ frontend/frontend/src/component/image.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+function Image() {
+
+    const [file,setFile]=useState()
+    const [image, setImage] = useState()
+
+    const handleUpload =(e)=>{
+        const formdata = new FormData()
+        formdata.append( 'file', file)
+        axios.post('http://localhost:8080/upload', formdata)
+        .then(res=>console.log(res))
+        .catch(err=>console.log(err))
+    }
+
+    useEffect(()=>{
+        axios.get('http://localhost:8080/getimage')
+        .then(res=>setImage(res.data[1].image))
+        .catch(err=>console.log(err))
+    },[])
+
+
+  return (
+    <div>
+      <input type='file' onChange={e=>setFile(e.target.files[0])}/>
+      <button onClick={handleUpload}>Upload</button>
+      <br/>
+      <img src= {'http://localhost:8080/images/'+image} alt='image'/>
+    </div>
+  )
+}
+
+export default Image
